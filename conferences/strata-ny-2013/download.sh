2a4799fc5ff5b4f970c787563c4bf4be94a36a57
Thomas Levine <occurrence@thomaslevine.com>
2013-04-17 12:38:21 -0400

    strata 2013

---
+++ conferences/strata-ny-2013/download.sh
@@ -0,0 +1,3 @@
+wget -O themes.htm 'http://strataconf.com/stratany2013?intcmp=il-strata-stny13-franchise-page'
+wget -O call-for-proposals.htm http://strataconf.com/stratany2013/public/cfp/264
+echo Log in to '"http://strataconf.com/stratany2013/user/proposal/propose/cfp/264"' for the submission form.
