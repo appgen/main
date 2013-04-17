3063c30fe6faaebc0aaaa33f6f8b2916ecde2319
Thomas Levine <occurrence@thomaslevine.com>
2013-04-17 12:35:07 -0400

    datagotham submission

---
+++ conferences/datagotham-2013/download.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget -O call-for-proposals.htm http://www.datagotham.com/call-for-proposals/
+wget -O submission-form.htm 'https://docs.google.com/forms/d/1HjaTj9HudJfmJWGWYu7B68XPzITdiwW0FcjyyccBUiU/viewform?embedded=true&pli=1'
